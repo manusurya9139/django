6a33395bf62d7b08a046a2d2c4af8135aa309bfb
Manoj <manusurya9139@gmail.com>
2015-05-17 12:38:04 +0530

    adding first project

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/manoj/django-manoj/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.8.1','django-admin.py'
+__requires__ = 'Django==1.8.1'
+import pkg_resources
+pkg_resources.run_script('Django==1.8.1', 'django-admin.py')
